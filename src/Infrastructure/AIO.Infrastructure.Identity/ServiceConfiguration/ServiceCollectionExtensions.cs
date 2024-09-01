5bbb2a6e1a725aebfe42a13545e787325d29f124
Ali Sedighian <ali.sedighian.kashi@gmail.com>
2024-09-01 18:36:32 +0330

    feat: add AIO.Infrastructure.Identity project

---
+++ src/Infrastructure/AIO.Infrastructure.Identity/ServiceConfiguration/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Security.Claims;
+using System.Text;
+
+namespace AIO.Infrastructure.Identity.ServiceConfiguration;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services)
+    {
+        
+        return services;
+    }
+}
